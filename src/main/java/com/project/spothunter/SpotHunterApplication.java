27d7d087b44fb7a757d6a4b0c5abddb7f491159f
Bruin324 <bruin324@gmAIL.COM>
2017-08-29 10:55:47 -0400

    initial commit

---
+++ src/main/java/com/project/spothunter/SpotHunterApplication.java
@@ -0,0 +1,12 @@
+package com.project.spothunter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotHunterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotHunterApplication.class, args);
+	}
+}
